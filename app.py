de67f5974acdc1005d83025f5ced0d4237c86860
Sanjana02562 <ersanjanaraj.work@gmail.com>
2025-09-24 19:26:24 +0530

    Fix: load .env in app.py and add flask-migrate to requirements

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from dotenv import load_dotenv
 import os
 from portfolio import create_app
 
-# Load .env variables
+# Load .env from your project root
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv()
+load_dotenv(dotenv_path)
+
+# Debug
+print("DEBUG EMAIL_USER =", os.getenv("EMAIL_USER"))
 
 app = create_app()
 
